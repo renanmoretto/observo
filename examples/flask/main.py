a3d1bf4c4831fad98aa0a1ff5fc682b5298b61dc
Renan Moretto <renan_morettopereira@hotmail.com>
2025-04-03 11:42:16 -0300

    added example + patch

---
+++ examples/flask/main.py
@@ -0,0 +1,19 @@
+import observo
+from flask import Flask
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+
+app = Flask(__name__)
+
+observo.watch_logs()
+observo_app = observo.get_wsgi_app()
+
+app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {'/observo': observo_app})
+
+
+@app.route('/')
+def index():
+    return 'Hello, World!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
