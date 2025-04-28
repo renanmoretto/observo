e238787460f1e256c3b3bce1f643e12646d40dc9
Renan Moretto <renan_morettopereira@hotmail.com>
2025-04-28 19:42:53 -0300

    fastapi example

---
+++ examples/fastapi/main.py
@@ -0,0 +1,15 @@
+import observo as observo
+from fastapi import FastAPI
+
+app = FastAPI()
+
+observo.set_password('1234')
+observo.watch_logs()
+observo_asgi_app = observo.get_asgi_app()
+
+app.mount('/observo', observo_asgi_app)
+
+
+@app.get('/')
+async def index():
+    return 'Hello, World!'
